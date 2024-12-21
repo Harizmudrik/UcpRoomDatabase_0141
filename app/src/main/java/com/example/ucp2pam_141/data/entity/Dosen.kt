186236a93665050ebf240daf8ab3147844a233e6
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 05:11:23 +0700

    Penambahan dalam "Entity Dosen" sesuai dengan kebutuhan yang diminta

--- app/src/main/java/com/example/ucp2pam_141/data/entity/Dosen.kt
+++ app/src/main/java/com/example/ucp2pam_141/data/entity/Dosen.kt
@@ -1,5 +1,12 @@
 package com.example.ucp2pam_141.data.entity
 
-class Dosen {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-}
+@Entity(tableName = "dosen")
+class Dosen (
+    @PrimaryKey
+    val nidn: String,
+    val nama: String,
+    val jenisKelamin: String,
+)
