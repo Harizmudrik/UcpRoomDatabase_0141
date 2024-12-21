ae40b509086be79c68633370c0d06bbecd7fb54b
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 03:31:46 +0700

    Penambahan dalam "dao" untuk bisa memasukan dan mengambil berdasarkan nama

--- app/src/main/java/com/example/ucp2pam_141/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2pam_141/data/dao/DosenDao.kt
@@ -1,4 +1,20 @@
 package com.example.ucp2pam_141.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.ucp2pam_141.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface DosenDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun InsertDosen(dosen: Dosen)
+
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+
 }
