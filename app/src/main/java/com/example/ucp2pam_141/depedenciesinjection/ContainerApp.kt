71249219738bfa4bfdafc0de6772413accaf83f2
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 12:15:31 +0700

    menambahkan untuk "Matakuliah", menginisialisasi dan mengakses repository mata kuliah secara lazy

--- app/src/main/java/com/example/ucp2pam_141/depedenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam_141/depedenciesinjection/ContainerApp.kt
@@ -1,2 +1,19 @@
 package com.example.ucp2pam_141.depedenciesinjection
 
+import android.content.Context
+import com.example.ucp2pam_141.data.database.KrsDatabase
+import com.example.ucp2pam_141.repository.LocalRepositoryMatakuliah
+import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+
+interface InterfaceContainerApp {
+    val repositoryMatakuliah: RepositoryMatakuliah
+
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp{
+    override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
+        LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
+    }
+
+
+}
