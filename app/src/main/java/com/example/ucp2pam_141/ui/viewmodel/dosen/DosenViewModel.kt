c7bf22f1823fa8bc801d2ba931e0b251d6bf9435
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:06:01 +0700

    memperbarui status uiState dengan data baru dari DosenEvent

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DosenViewModel.kt
@@ -1,8 +1,19 @@
 package com.example.ucp2pam_141.ui.viewmodel.dosen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.ucp2pam_141.repository.RepositoryDosen
 
 class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
+
+    var uiState by mutableStateOf(DosenUIState())
+
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent,
+        )
+    }
 
 }
