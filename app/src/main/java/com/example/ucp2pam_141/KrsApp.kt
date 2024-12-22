4964d64d39408bf7ad448d06d4e021e04376a10a
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 15:51:19 +0700

    menambahkan fungsi yang dipanggil pertama kali ketika aplikasi diluncurkan.

--- app/src/main/java/com/example/ucp2pam_141/KrsApp.kt
+++ app/src/main/java/com/example/ucp2pam_141/KrsApp.kt
@@ -1,4 +1,14 @@
 package com.example.ucp2pam_141
 
-class KrsApp {
+import android.app.Application
+import com.example.ucp2pam_141.depedenciesinjection.ContainerApp
+
+class KrsApp : Application() {
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+
+        containerApp = ContainerApp(this)
+    }
 }
