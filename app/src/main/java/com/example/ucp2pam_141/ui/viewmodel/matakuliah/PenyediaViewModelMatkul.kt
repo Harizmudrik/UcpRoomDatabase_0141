e2797503f19a5978a5c0d2f653775c54235c7555
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 15:01:43 +0700

    menambahkan "fun CreationExtras" yang digunakan untuk menyediakan informasi tambahan yang diperlukan saat membuat ViewModel.

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/PenyediaViewModelMatkul.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/PenyediaViewModelMatkul.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2pam_141.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 
@@ -32,3 +34,6 @@ object PenyediaViewModelMatkul {
         }
     }
 }
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
