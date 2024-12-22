80c1895af0a670a3f2237e67ae0ebc410b03da8d
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:05:01 +0700

    kelas untuk mengelola data Dosen

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DosenViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ucp2pam_141.ui.viewmodel.dosen
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryDosen
+
+class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
+
+}
