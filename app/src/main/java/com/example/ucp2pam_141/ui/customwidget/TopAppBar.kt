e3ada91ef25f87eca06fc8bfee50440bba993fcf
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 16:36:36 +0700

    pembuatan customwidget yang digunakan untuk menyimpan komponen UI kustom yang dapat digunakan ulang di seluruh aplikasi dan composable function yang membuat sebuah AppBar (atau bar bagian atas) dengan judul dan tombol kembali (Back Button

--- app/src/main/java/com/example/ucp2pam_141/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,15 @@
 package com.example.ucp2pam_141.ui.customwidget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier = Modifier
+) {
+
+}
