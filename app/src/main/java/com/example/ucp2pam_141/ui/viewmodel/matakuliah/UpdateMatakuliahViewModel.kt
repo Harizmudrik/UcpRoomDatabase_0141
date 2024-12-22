ce0211d275cc068b12570ab7c861528d62d13d8a
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 15:04:55 +0700

    menambahkan updateUiState yang berfungsi untuk menyimpan data yang diperlukan UI

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/UpdateMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/UpdateMatakuliahViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.ucp2pam_141.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.ucp2pam_141.repository.RepositoryMatakuliah
@@ -8,5 +9,6 @@ class UpdateMatakuliahViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMatakuliah: RepositoryMatakuliah
 ) : ViewModel() {
-
+    var updateUiState by mutableStateOf(MatakuliahUIState())
+        private set
 }
