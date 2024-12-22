105a21445c8d78dcfd6b522d789143e28da2d83b
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 14:49:37 +0700

    membuat ViewModel yang digunakan untuk mengelola data detail mata kuliah

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/DetailMatakuliahViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2pam_141.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+
+class DetailMatakuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMatakuliah: RepositoryMatakuliah,
+) : ViewModel() {
+
+
+}
