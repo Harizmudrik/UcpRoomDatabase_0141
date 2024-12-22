e628f1a8165cb5a2652614138c356c2bd5a56ed9
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 12:01:19 +0700

    Membuat "LocalRepositoryDosen" yang bertugas untuk menjembatani antara DosenDao dan komponen lain dalam aplikasi.

---
+++ app/src/main/java/com/example/ucp2pam_141/repository/LocalRepositoryDosen.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2pam_141.repository
+
+import com.example.ucp2pam_141.data.dao.DosenDao
+import kotlinx.coroutines.flow.Flow
+
+
+class LocalRepositoryDosen (
+    private val dosenDao: DosenDao
+) : RepositoryDosen {
+
+    override suspend fun insertDosen(dosen: Dosen) {
+        dosenDao.InsertDosen(dosen)
+    }
+
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+
+
+}
