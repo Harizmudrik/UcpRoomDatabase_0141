590c1f5be38abfb89360b925a0d3309bf173fa0c
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 15:06:31 +0700

    menambahkan fungsi untuk mengonversi objek Matakuliah dari database ke MatakuliahUIState.

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/UpdateMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/UpdateMatakuliahViewModel.kt
@@ -3,7 +3,11 @@ package com.example.ucp2pam_141.ui.viewmodel.matakuliah
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMatakuliahViewModel(
     savedStateHandle: SavedStateHandle,
@@ -11,4 +15,16 @@ class UpdateMatakuliahViewModel(
 ) : ViewModel() {
     var updateUiState by mutableStateOf(MatakuliahUIState())
         private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiMatakuliahUpdate.KODE])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMatakuliah.getMatakuliah(_kode)
+                .filterNotNull()
+                .first()
+                .toUiStateMatakuliah()
+        }
+    }
+
 }
