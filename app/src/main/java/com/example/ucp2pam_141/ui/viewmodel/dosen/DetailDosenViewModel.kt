96519f628cb79c6d21b3a2b3253e4e46f9a5cf24
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:43:27 +0700

    menambahkan class DetailDosenViewModel untuk menampilkan detail data dosen berdasarkan NIDN

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2pam_141.ui.viewmodel.dosen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryDosen
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+) : ViewModel() {
+
+}
