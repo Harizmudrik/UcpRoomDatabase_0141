d9aa52dee1420d93baa57955c890d5ef0f4e5e8c
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 12:16:07 +0700

    menambahkan untuk "Dosen", menginisialisasi dan mengakses repository Dosen secara lazy

--- app/src/main/java/com/example/ucp2pam_141/depedenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam_141/depedenciesinjection/ContainerApp.kt
@@ -2,12 +2,14 @@ package com.example.ucp2pam_141.depedenciesinjection
 
 import android.content.Context
 import com.example.ucp2pam_141.data.database.KrsDatabase
+import com.example.ucp2pam_141.repository.LocalRepositoryDosen
 import com.example.ucp2pam_141.repository.LocalRepositoryMatakuliah
+import com.example.ucp2pam_141.repository.RepositoryDosen
 import com.example.ucp2pam_141.repository.RepositoryMatakuliah
 
 interface InterfaceContainerApp {
     val repositoryMatakuliah: RepositoryMatakuliah
-
+    val repositoryDosen: RepositoryDosen
 }
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp{
@@ -15,5 +17,7 @@ class ContainerApp(private val context: Context) : InterfaceContainerApp{
         LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
     }
 
-
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
+    }
 }
