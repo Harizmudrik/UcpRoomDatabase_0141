c2b0c1d10f5e8b6c150794a5b0b464fcbf742979
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 16:24:58 +0700

    membuat object yang setiap objeknya mengimplementasikan AlamatNavigasi akan mengoverride properti route untuk mendefinisikan rute masing-masing.

--- app/src/main/java/com/example/ucp2pam_141/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,37 @@
 package com.example.ucp2pam_141.ui.navigation
 
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home_menu"
+}
+
+object DestinasiDosen : AlamatNavigasi {
+    override val route = "dosen"
+}
+object DestinasiInsertMataKuliah : AlamatNavigasi{
+    override val route: String = "insert_matakuliah"
+}
+object DestinasiDosenDetail : AlamatNavigasi {
+    override val route = "dosendetail"
+    const val NIDN = "nidn"
+    val routesWithArg = "$route/{$NIDN}"
+}
+
+object DestinasiMatakuliah : AlamatNavigasi {
+    override val route = "matakuliah"
+}
+
+object DestinasiMatakuliahDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiMatakuliahUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
