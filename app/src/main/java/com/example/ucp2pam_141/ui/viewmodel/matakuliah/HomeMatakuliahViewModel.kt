2dc38a9475ab04d982f37742c825f5f3e2eec869
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 14:42:56 +0700

    mengambil data matakuliah dari repositori dan mengelola aliran data menggunakan operator Flow

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
@@ -1,12 +1,47 @@
 package com.example.ucp2pam_141.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 // ViewModel untuk halaman utama mata kuliah
 class HomeMatkulViewModel(
     private val repositoryMatakuliah: RepositoryMatakuliah
 ) : ViewModel() {
 
-
+    // StateFlow untuk mengelola state UI halaman utama mata kuliah
+    val homeUiState: StateFlow<HomeUiState> = repositoryMatakuliah.getAllMatakuliah() // Mengambil semua data mata kuliah
+        .filterNotNull() // Mengabaikan nilai null dari aliran data
+        .map {
+            // Mengonversi daftar mata kuliah menjadi state UI
+            HomeUiState(
+                listMatakuliah = it.toList(), // Data list mata kuliah
+                isLoading = false, // Status loading selesai
+            )
+        }
+        .onStart {
+            // Menangani status awal saat data mulai dimuat
+            emit(HomeUiState(isLoading = true)) // Emit state dengan loading aktif
+            delay(900) // Menunda selama 900ms untuk memberikan efek loading
+        }
+        .catch {
+            // Menangani error dan mengubah state ke status error
+            emit(
+                HomeUiState(
+                    isLoading = false, // Loading dihentikan
+                    isError = true, // Tandai adanya error
+                    errorMessage = it.message ?: "Terjadi Kesalahan" // Pesan error default
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope, // Coroutine scope yang terkait dengan lifecycle ViewModel
+            started = SharingStarted.WhileSubscribed(5000), // Aliran aktif selama 5000ms setelah tidak ada subscriber
+            initialValue = HomeUiState( // Nilai awal state
+                isLoading = true, // Mulai dengan status loading
+            )
+        )
 }
