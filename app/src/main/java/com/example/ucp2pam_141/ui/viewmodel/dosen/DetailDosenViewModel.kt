162c11490fbaff3a7930bd60eebb1a2867749bf7
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:46:58 +0700

    sama seperti yang lain, mengambil data dosen dari repositori . Mengelola aliran data menggunakan operator Flow

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -2,7 +2,16 @@ package com.example.ucp2pam_141.ui.viewmodel.dosen
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam_141.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailDosenViewModel(
     savedStateHandle: SavedStateHandle,
@@ -11,4 +20,33 @@ class DetailDosenViewModel(
 
     private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDosenDetail.NIDN])
 
+    val detailUiState: StateFlow<DetailUiState> = repositoryDosen.getDosen(_nidn)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue =
+            DetailUiState(isLoading = true)
+        )
+
 }
+
