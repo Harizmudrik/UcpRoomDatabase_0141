6034a0e8a266d5d879bb6491a2e04a5d8c6a4c4c
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:59:04 +0700

    menambahkan fungsi untuk memvalidasi apakah semua kolom input sudah terisi.

--- app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+import com.example.ucp2pam_141.ui.viewmodel.dosen.FormErrorState
 
 class MatakuliahViewModel(private val repositoryMatakuliah: RepositoryMatakuliah) : ViewModel() {
 
@@ -15,4 +16,20 @@ class MatakuliahViewModel(private val repositoryMatakuliah: RepositoryMatakuliah
             matakuliahEvent = matakuliahEvent,
         )
     }
+
+    private fun validateFields(): Boolean {
+        val event = uiState.matakuliahEvent
+        val errorState = FormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            DosenPengampu = if (event.DosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
 }
+
