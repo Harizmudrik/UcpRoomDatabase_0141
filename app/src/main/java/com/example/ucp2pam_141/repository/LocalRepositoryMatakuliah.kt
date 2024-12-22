f1b90b744c8429e4fbcdadf17dabcce5f2e95412
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 12:07:46 +0700

    menambahkan beberapa fungsi untuk menghubungkan MatakuliahDao dengan aplikasi lainnya untuk pengelolaan data matakuliah.

--- app/src/main/java/com/example/ucp2pam_141/repository/LocalRepositoryMatakuliah.kt
+++ app/src/main/java/com/example/ucp2pam_141/repository/LocalRepositoryMatakuliah.kt
@@ -1,4 +1,20 @@
 package com.example.ucp2pam_141.repository
 
-class LocalRepositoryMatakuliah {
-}
+import com.example.ucp2pam_141.data.dao.MatakuliahDao
+import com.example.ucp2pam_141.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMatakuliah (
+    private val matakuliahDao: MatakuliahDao
+) : RepositoryMatakuliah {
+    override suspend fun insertMatakuliah(matakuliah: Matakuliah) {
+        matakuliahDao.insertMatakuliah(matakuliah)
+    }
+
+    override fun getAllMatakuliah(): Flow<List<Matakuliah>> {
+        return matakuliahDao.getAllMatakuliah()
+    }
+
+    override fun getMatakuliah(kode: String): Flow<Matakuliah> {
+        return matakuliahDao.getMatakuliah(kode)
+    }
