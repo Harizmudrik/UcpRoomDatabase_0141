2b08073ad990c4d839a9cf5230a63bd90016d52f
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 14:38:11 +0700

    membuat "HomeMatakuliahViewModel" digunakan untuk mengelola logika bisnis terkait halaman utama matakuliah.

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/HomeMatakuliahViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2pam_141.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+
+// ViewModel untuk halaman utama mata kuliah
+class HomeMatkulViewModel(
+    private val repositoryMatakuliah: RepositoryMatakuliah
+) : ViewModel() {
+
+
+}
