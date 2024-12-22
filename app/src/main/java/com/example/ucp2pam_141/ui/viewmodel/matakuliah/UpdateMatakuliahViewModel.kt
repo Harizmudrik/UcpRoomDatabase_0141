c870a71f78668b4964451680a9c6548ba5cc7ebb
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 15:03:41 +0700

    membuat class update yang digunakan untuk mengubah matakuliah

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/matakuliah/UpdateMatakuliahViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2pam_141.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryMatakuliah
+
+class UpdateMatakuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMatakuliah: RepositoryMatakuliah
+) : ViewModel() {
+
+}
