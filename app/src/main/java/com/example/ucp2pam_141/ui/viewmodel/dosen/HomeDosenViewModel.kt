a8adf479f83dcca87800dae0fd00dda6d1b869c8
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-22 13:30:40 +0700

    menambahkan class untuk halaman atau komponen yang menampilkan daftar dosen.

---
+++ app/src/main/java/com/example/ucp2pam_141/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2pam_141.ui.viewmodel.dosen
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam_141.repository.RepositoryDosen
+
+class HomeDosenViewModel (
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+}
